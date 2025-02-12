e1528c0c3ce5acccb9f905b9cdad5fcfc46a9219
M1keyy <miguel.alcantar@cetys.edu.mx>
2025-02-12 15:17:55 -0800

    Configuración del react router en main.tsx

--- frontend/to-do-list/src/main.tsx
+++ frontend/to-do-list/src/main.tsx
@@ -1,7 +1,32 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router";
+import HomePage from "./pages/HomePage.tsx";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="Home" replace />
+      },
+      {
+        path: "Home",
+        element: <HomePage />
+      }
+    ]
+  }
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <>
+    <RouterProvider router={router} />
+  </>
+);
