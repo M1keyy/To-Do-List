4bcdd97187b2fc5ba37d6ed129bed45ed5557e4b
M1keyy <miguel.alcantar@cetys.edu.mx>
2025-02-13 12:15:31 -0800

    Argumento onClick agregado al componente AddButton

--- frontend/to-do-list/src/components/AddButton.tsx
+++ frontend/to-do-list/src/components/AddButton.tsx
@@ -1,8 +1,8 @@
 import { IoIosAdd } from "react-icons/io";
 
-const AddButton = () => {
+const AddButton = (props: { onClick?: () => void }) => {
   return (
-    <button className="rounded-xl bg-slate-800 hover:brightness-150 transition-all cursor-pointer shadow shadow-gray-800 p-4 w-full justify-items-center">
+    <button onClick={props.onClick} className="rounded-xl bg-slate-800 hover:brightness-150 transition-all cursor-pointer shadow shadow-gray-800 p-4 w-full justify-items-center">
       <IoIosAdd size={48} />
     </button>
   );
