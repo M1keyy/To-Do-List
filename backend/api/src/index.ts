ebc7e3cad1ad4a16b3ad4add7eebee7748158e6f
M1keyy <miguel.alcantar@cetys.edu.mx>
2025-02-13 17:29:30 -0800

    Modificación del index para la creación de la tabla Task en la base de datos. Se comienzan a crear los endpoints.

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { Task } from "./models/tasks";
+import sequelize from "./configs/datablase";
 
 dotenv.config();
 
@@ -9,9 +11,23 @@ const PORT: string = process.env.PORT || "3000";
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hola, Express con TypeScript!");
+  res.send("Este es el API para el To-Do List!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// GET /tasks → Obtener todas las tareas.
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.findAll();
+  res.json(tasks);
 });
+
+// GET /tasks/:id → Obtener una tarea específica.
+// POST /tasks → Crear una nueva tarea.
+// PUT /tasks/:id → Actualizar una tarea existente.
+// DELETE /tasks/:id → Eliminar una tarea.
+
+sequelize.sync({ alter: true }).then(() => {
+    console.log("Base de datos conectada 🚀");
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+  });
