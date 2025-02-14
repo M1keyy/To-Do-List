7c23362eaf1c08fb507b3b6148b54644c36080cb
M1keyy <miguel.alcantar@cetys.edu.mx>
2025-02-14 14:26:35 -0800

    Se modificó la vista principal HomePage para que despliegue el componente TasksList en vez de la lista de tareas con datos falsos para pruebas.

--- frontend/to-do-list/src/pages/HomePage.tsx
+++ frontend/to-do-list/src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import AddButton from "../components/AddButton";
-import Card from "../components/Card";
-import tasks from "../utils/testData";
+import TaskList from "../components/TasksList";
 
 const HomePage = () => {
   return (
@@ -8,17 +7,7 @@ const HomePage = () => {
       <div className="flex flex-col gap-4 p-4 items-center mx-auto max-w-3xl pb-12">
         <h1 className="font-bold font-mono text-5xl p-4 pt-10 w-full">To-Do List</h1>
         <AddButton />
-        {tasks.map((task) => (
-          <Card
-            key={task.id}
-            id={task.id}
-            title={task.title}
-            description={task.description}
-            status={task.status}
-            createdAt={task.createdAt}
-            updatedAt={task.updatedAt}
-          />
-        ))}
+        <TaskList />
       </div>
     </>
   );
