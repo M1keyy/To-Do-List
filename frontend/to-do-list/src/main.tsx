b77156756540e78cf0daac1c850d2ef4487c84df
M1keyy <miguel.alcantar@cetys.edu.mx>
2025-02-14 11:23:24 -0800

    Instalación y configuración inicial de react query en /src/main.tsx

--- frontend/to-do-list/src/main.tsx
+++ frontend/to-do-list/src/main.tsx
@@ -1,12 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 import HomePage from "./pages/HomePage.tsx";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -15,18 +14,20 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Navigate to="Home" replace />
+        element: <Navigate to="Home" replace />,
       },
       {
         path: "Home",
-        element: <HomePage />
-      }
-    ]
-  }
+        element: <HomePage />,
+      },
+    ],
+  },
 ]);
 
 createRoot(document.getElementById("root")!).render(
   <>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </>
 );
