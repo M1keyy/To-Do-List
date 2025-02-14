c79018a8d22004b42a7c2841466f033b0e6c295a
M1keyy <miguel.alcantar@cetys.edu.mx>
2025-02-14 14:23:30 -0800

    Se modificó useTasks para que retornara useQuery correctamente

--- frontend/to-do-list/src/hooks/useTasks.ts
+++ frontend/to-do-list/src/hooks/useTasks.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import fetchTasks from "../api/fetchTasks";
 
 export const useTasks = () => {
-  useQuery({
+  return useQuery({
     queryKey: ["tasks"],
     queryFn: fetchTasks,
   });
