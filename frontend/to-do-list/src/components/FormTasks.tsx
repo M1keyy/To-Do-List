b1ed991c93575ec50db85cf928bdfb538a9eeb99
M1keyy <miguel.alcantar@cetys.edu.mx>
2025-02-15 19:49:56 -0800

    Avance en el formulario. Ahora se implementaron validaciones, se le hizo mejoras visuales y se hicieron ajustes para que se reinicien los valores del formulario cuando se cancela. Además ya puede subir tareas. Falta modificar el input de Estado de la tarea para que sea más intuitivo y no de texto puro.

--- frontend/to-do-list/src/components/FormTasks.tsx
+++ frontend/to-do-list/src/components/FormTasks.tsx
@@ -1,59 +1,123 @@
 import { SubmitHandler, useForm } from "react-hook-form";
-import useAddTask from "../hooks/useAddTask";
 import Modal from "./Modal";
 import Task from "../interfaces/TaskInterface";
+import { useState } from "react";
+import { UseMutateAsyncFunction } from "@tanstack/react-query";
 
 const FormTask = (props: {
-  onSubmit?: () => void;
   open: boolean;
-  onClose?: () => void;
+  onClose: () => void;
+  mutationFn: UseMutateAsyncFunction<
+    any,
+    Error,
+    Omit<Task, "createdAt" | "updatedAt" | "id">,
+    unknown
+  >;
+  defaultValues?: Task;
 }) => {
-  const { mutateAsync: addTaskMutation } = useAddTask({
-    title: "TEST DESDE FRONT",
-    status: "pending",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga qui quis ipsa quibusdam consectetur impedit natus exercitationem assumenda corrupti. Nisi, numquam quaerat maiores nesciunt corporis autem beatae facere? Rerum, et.",
-  });
-
-  type Inputs = Omit<Task, "createdAt" | "updatedAt" | "id">;
+  const [descLength, setDescLength] = useState<number>(
+    props.defaultValues?.description?.length || 0
+  );
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
-    if (props.onClose) props.onClose();
+    setError,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<Omit<Task, "createdAt" | "updatedAt">>({
+    defaultValues: {
+      title: props.defaultValues?.title || "",
+      description: props.defaultValues?.description || "",
+      status: props.defaultValues?.status || "pending",
+    },
+  });
+
+  const onSubmit: SubmitHandler<Omit<Task, "createdAt" | "updatedAt">> = async (
+    data
+  ) => {
+    try {
+      await props.mutationFn(data).then(() => props.onClose());
+    } catch (e) {
+      console.error(e);
+      setError("root", {
+        message: `Error al agregar tarea. ${e}`,
+      });
+    }
   };
 
   return (
-    <Modal open={props.open} onClose={props.onClose}>
+    <Modal
+      open={props.open}
+      onClose={() => {
+        setDescLength(0);
+        reset();
+        props.onClose();
+      }}
+    >
       <form
-        className="min-w-96 p-4 flex flex-col gap-4"
+        className="max-w-3xl w-screen p-4 flex flex-col gap-4"
         onSubmit={handleSubmit(onSubmit)}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            event.preventDefault();
+          }
+        }}
       >
-        <h1 className="text-center text-3xl font-mono font-semibold">
+        <h1 className="text-center text-4xl font-mono font-semibold">
           Agregar Tarea
         </h1>
-        <div className="grow w-full flex flex-col gap-4 text-xl">
+        <div className="grow w-full flex flex-col gap-4 text-xl p-4">
+          <label className="font-mono text-2xl font-semibold">Titulo:</label>
           <input
+            className="w-full bg-slate-700 p-4 rounded-2xl"
             {...register("title", {
               required: "El título es requerido",
+              maxLength: {
+                value: 20,
+                message: "El título no puede tener más de 20 caracteres",
+              },
             })}
             type="text"
-            placeholder="Título"
+            placeholder="Título de tu tarea"
           />
           {errors.title && (
             <div className="text-red-300">{errors.title.message}</div>
           )}
-          <input
-            {...register("description", {
-              maxLength: 500,
-            })}
-            type="text"
-            placeholder="Descripción"
-          />
+          <label className="font-mono text-2xl font-semibold">
+            Descripción:
+          </label>
+          <div className="w-full h-52 flex flex-col bg-slate-700 rounded-2xl">
+            <textarea
+              className="w-full grow p-4 bg-slate-700 rounded-t-2xl h-full"
+              {...register("description", {
+                maxLength: {
+                  value: 500,
+                  message:
+                    "La descripción no puede tener más de 500 caracteres",
+                },
+              })}
+              onChange={(input) => setDescLength(input.target.value.length)}
+              placeholder="Describe tu tarea"
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  e.stopPropagation();
+                }
+              }}
+            />
+            <div
+              className={`text-end px-4 pb-4 pt-1 ${
+                descLength > 500 ? "text-red-400" : ""
+              }`}
+            >
+              {descLength}/500
+            </div>
+          </div>
+
+          {errors.description && (
+            <div className="text-red-300">{errors.description.message}</div>
+          )}
+          <label className="font-mono text-2xl font-semibold">Estado:</label>
           <input
             {...register("status", {
               required: "El estado es requerido",
@@ -67,18 +131,28 @@ const FormTask = (props: {
         </div>
         <div className="flex flex-row gap-4 p-2 w-full">
           <button
-            className="p-2 bg-slate-700 rounded-xl grow text-xl font-mono font-semibold hover:brightness-150 cursor-pointer transition-all"
-            onClick={props.onClose}
+            className="p-2 bg-slate-700 rounded-xl grow text-2xl font-mono font-semibold hover:brightness-150 cursor-pointer transition-all"
+            disabled={isSubmitting}
+            onClick={() => {
+              setDescLength(0);
+              reset();
+              props.onClose();
+            }}
+            type="button"
           >
             Cancelar
           </button>
           <button
-            className="p-2 bg-green-600 rounded-xl grow text-xl font-mono font-semibold hover:brightness-150 cursor-pointer transition-all"
+            className="p-2 bg-green-600 rounded-xl grow text-2xl font-mono font-semibold hover:brightness-150 cursor-pointer transition-all"
             type="submit"
+            disabled={isSubmitting}
           >
-            Confirmar
+            {isSubmitting ? "Subiendo..." : "Confirmar"}
           </button>
         </div>
+        {errors.root && (
+          <div className="text-red-300">{errors.root.message}</div>
+        )}
       </form>
     </Modal>
   );
--- frontend/to-do-list/src/pages/HomePage.tsx
+++ frontend/to-do-list/src/pages/HomePage.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import AddButton from "../components/AddButton";
 import FormTask from "../components/FormTasks";
 import TaskList from "../components/TasksList";
+import useAddTask from "../hooks/useAddTask";
 
 const HomePage = () => {
 
   const [open, setOpen] = useState<boolean>(false);
 
+  const { mutateAsync: addTaskMutation } = useAddTask();
+  
   return (
     <>
       <div className="flex flex-col gap-4 p-4 items-center mx-auto max-w-3xl pb-12">
@@ -14,7 +17,7 @@ const HomePage = () => {
           To-Do List
         </h1>
         <AddButton onClick={() => setOpen(true)} />
-        <FormTask open={open} onClose={() => setOpen(false)} />
+        <FormTask open={open} onClose={() => setOpen(false)} mutationFn={addTaskMutation} />
         <TaskList />
       </div>
     </>
